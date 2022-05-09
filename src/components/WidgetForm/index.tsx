cb42193bf233f5aefe7f67c40b946550819ceeed
Anderson Mateus <sinothic@gmail.com>
2022-05-09 20:00:54 -0300

    Initial Commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideiaImageUrl from "../../assets/idea.svg";
+import otherImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de uma minhoca roxa",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideiaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: otherImageUrl,
+      alt: "Imagem de uma nuvem azul",
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeedbackSent] = useState(false);
+
+  function handleRestartFeedback() {
+    setFeedbackSent(false);
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {feedbackSent ? (
+        <FeedbackSuccessStep
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSent={() => setFeedbackSent(true)}
+            />
+          )}
+        </>
+      )}
+      <footer className="text-xs">Feito com 🤍 por Anderson Mateus</footer>
+    </div>
+  );
+}
